3958f90ee0bf6a40017cd75483eb6731d88ecc89
Dlean Jeans <dleanjeans@gmail.com>
2017-03-29 17:32:34 +0700

    Rename ScriptClassUtil -> ClassUtil

--- hscript-plus/test/TestAll.hx
+++ hscript-plus/test/TestAll.hx
@@ -3,7 +3,7 @@ package;
 import utest.Runner;
 import utest.ui.Report;
 
-import cases.ScriptClassUtilTest;
+import cases.ClassUtilTest;
 import cases.ScriptStateTest;
 import cases.ParserTest;
 import cases.InterpTest;
@@ -19,7 +19,7 @@ class TestAll {
 	}
 
 	static function addTests(runner:Runner) {
-		runner.addCase(new ScriptClassUtilTest());
+		runner.addCase(new ClassUtilTest());
 		runner.addCase(new ScriptStateTest());
 		runner.addCase(new ParserTest());
 		runner.addCase(new InterpTest());
